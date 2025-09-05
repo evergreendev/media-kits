d0e1d671bc0cbdfce20ae143d9f8a4b00fd65a6a
Joe simpson <joe@egmrc.com>
2025-09-05 09:21:34 -0600

    Add ExpandableSection component and integrate into Media Kit page

---
+++ app/components/ExpandableSection/index.tsx
@@ -0,0 +1,47 @@
+"use client"
+import {useState} from "react";
+import {StaticImageData} from "next/image";
+import {LucideSquareChevronDown} from "lucide-react";
+import Image from "next/image";
+
+type Props = {
+    title: string,
+    description?: string,
+    price: string,
+    icon?: StaticImageData,
+
+}
+
+const ExpandableSection = ({title, description, price, icon}: Props) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    return <button
+        className="text-center text-slate-900 flex items-center gap-5 bg-slate-100 p-4 border border-[#75b543]"
+        onClick={() => setIsExpanded(!isExpanded)}>
+        <div className="flex flex-col items-center">
+            {
+                icon &&
+                <Image src={icon} alt="Icon" width={150} height={150} className={"mx-auto mb-6"} priority={true}/>
+            }
+            <div className="flex items-center gap-2">
+                <div className="flex flex-col items-center">
+
+                    <h2 className="text-xl font-bold">{title}</h2>
+                    <p className="text-gray-400">(learn more)</p>
+                </div>
+                {
+                    isExpanded ? <LucideSquareChevronDown/> : <LucideSquareChevronDown className="-rotate-90"/>
+                }
+            </div>
+            <div className={`${isExpanded ? "max-h-screen" : "max-h-0 overflow-hidden"} transition-all duration-300`}>
+                <div className="w-full h-px bg-slate-300 my-4"/>
+                {
+                    description && <p>{description}</p>
+                }
+                <p className="text-lg font-bold">{price}</p>
+            </div>
+        </div>
+    </button>
+}
+
+export default ExpandableSection
--- app/faces/media-kit/page.tsx
+++ app/faces/media-kit/page.tsx
@@ -1,6 +1,11 @@
+import ExpandableSection from "@/app/components/ExpandableSection";
+import doubleSpread from "@/public/double-spread.png";
+
 const Page = () => {
 
-    return <div>Media Kit</div>
+    return <div>
+        <ExpandableSection icon={doubleSpread} title="Double Page Spread" price="$4,000" description={`16.75" x 10.75"`}/>
+    </div>
 }
 
 
